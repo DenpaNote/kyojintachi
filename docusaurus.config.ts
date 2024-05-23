382dc8be5955a49a6686e3ca0289d4cdf1fb8d60
yazawazi <47273265+Yazawazi@users.noreply.github.com>
2024-05-24 01:35:44 +0800

    feat: change config for github pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,12 +11,15 @@ const config: Config = {
   url: 'https://denpanote.moe',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/kjojintachi/',
+  baseUrl: '/kyojintachi',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'DenpaNote', // Usually your GitHub org/user name.
   projectName: 'kyojintachi', // Usually your repo name.
+  
+  deploymentBranch: "gh-pages",
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
